efc27984ac0e828170a0903b8b841b6cf045c13c
shawngmc <shawngmc@gmail.com>
2022-08-18 16:24:51 -0400

    task command improvements

--- src/gex/commands/tasks/details.py
+++ src/gex/commands/tasks/details.py
@@ -1,16 +1,20 @@
+import importlib
 import os
 import click
 from rich.console import Console
 from rich.markdown import Markdown
+
+from gex.lib.utils import helper
 
 @click.command()
 @click.option('--task', 'task', help = 'name of the transform set to run', required=True)
 def details(task):
     """Get details about a specific extraction task"""
 
-    # logging.basicConfig(stream=sys.stderr, level=logging.INFO)
     dir_path = os.path.dirname(os.path.realpath(__file__))
     md_path = os.path.join(dir_path, "..", "..", "..", "lib", "transforms", f'{task}.md')
+    transform_module = importlib.import_module(f'gex.lib.transforms.{task}')
+    helper.task_module_print_header(task, transform_module)
     if not os.path.exists(md_path):
         print(md_path)
         raise Exception("Task not found to describe.")
