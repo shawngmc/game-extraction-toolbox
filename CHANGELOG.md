304bb0f67c13f5322bf8cbde6f6347276327661d
shawngmc <shawngmc@gmail.com>
2022-08-18 10:23:10 -0400

    Restructure 'tasks' command, fix testing launch configs

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,10 @@
+# 0.0.21
+- Improvements
+  - Moved 'postprocess steam' commands to more generic 'tasks' command
+    This is one of the main purposes of this program, and not Steam specific - GOG, etc. should also be considered
+- Internal
+  - Test configs for development should now work on any machine
+
 # 0.0.20
 - Added Games/Collections:
   - Sega Genesis and Mega Drive Collection
--- src/gex/toolbox.py
+++ src/gex/toolbox.py
@@ -6,6 +6,7 @@ from gex.commands.archive.archive import archive
 from gex.commands.file.file import file
 from gex.commands.finder.finder import finder
 from gex.commands.postprocess.postprocess import postprocess
+from gex.commands.tasks.tasks import tasks
 from gex.commands.version import version
 
 logger = logging.getLogger('gextoolbox')
@@ -19,6 +20,7 @@ cli.add_command(archive)
 cli.add_command(file)
 cli.add_command(finder)
 cli.add_command(postprocess)
+cli.add_command(tasks)
 cli.add_command(version)
 
 if __name__ == '__main__':
