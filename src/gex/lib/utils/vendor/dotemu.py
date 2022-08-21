9beb0c1ccdf4b8c57a9f750d5e6f90d140be0e4b
shawngmc <shawngmc@gmail.com>
2022-08-21 00:16:42 -0400

    Add example layout to dotemu

--- src/gex/lib/utils/vendor/dotemu.py
+++ src/gex/lib/utils/vendor/dotemu.py
@@ -1,5 +1,19 @@
 import copy
 
+    # Example Layout
+    # _DDRAGON_TILE_LAYOUT = {
+    #     'width': 16,
+    #     'height': 16,
+    #     'total': [1,2],
+    #     'planes': 4,
+    #     'planeoffset': [[1,2,0], [1,2,4], 0, 4],
+    #     'xoffset': [3, 2, 1, 0, 16*8+3, 16*8+2, 16*8+1, 16*8+0,
+    #         32*8+3, 32*8+2, 32*8+1, 32*8+0, 48*8+3, 48*8+2, 48*8+1, 48*8+0],
+    #     'yoffset': [0*8, 1*8, 2*8, 3*8, 4*8, 5*8, 6*8, 7*8,
+    #         8*8, 9*8, 10*8, 11*8, 12*8, 13*8, 14*8, 15*8],
+    #     'charincrement': 64*8
+    # }
+
 def reencode_gfx(contents, layout):
     # This appears to be rebuilding GFX roms from sprites.
     modLayout = copy.deepcopy(layout)
