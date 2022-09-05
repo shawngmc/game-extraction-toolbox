2629f67923229d39e50128970698ef431c6c6efc
shawngmc <shawngmc@gmail.com>
2022-09-05 18:10:44 -0400

    Add CAS2 PH and fix CAS1 PH typos

--- src/gex/lib/tasks/impl/cas2.py
+++ src/gex/lib/tasks/impl/cas2.py
@@ -1,4 +1,4 @@
-'''INCOMPLETE Implementation of cas1: Capcom Arcade Stadium 1'''
+'''INCOMPLETE Implementation of cas2: Capcom Arcade Stadium 2'''
 import glob
 import logging
 import os
@@ -10,14 +10,14 @@ logger = logging.getLogger('gextoolbox')
 
 # TODO: Further research required
 
-class CAS1Task(BaseTask):
-    '''INCOMPLETE Implements cas1: Capcom Arcade Stadium 1'''
-    _task_name = 'cas1'
-    _title = "Capcom Arcade Stadium 1"
+class cas2Task(BaseTask):
+    '''INCOMPLETE Implements cas2: Capcom Arcade Stadium 2'''
+    _task_name = 'cas2'
+    _title = "Capcom Arcade Stadium 2"
     _details_markdown = '''
 ## INCOMPLETE!
 
-This script is a placeholder for full CAS1 extraction.
+This script is a placeholder for full cas2 extraction.
 
 Currently, it extracts nothing; however, with a "--prop include-partials=True", it will extract all KPKA paks with the following naming convention:
 [ARCHIVEID]_[SEQUENTIALNUM]_[SIZE].dat
@@ -28,32 +28,16 @@ These are not playable ROMs!
 These appear to be KPKA archives, and it is reasonably certain that the extraction is correct, as most files have a 3-4 character allcaps header
 indicating file type.
 
-#### Example KPKA (1556708)
- - 1556708_0_1957557.dat - One of the ROM packages
- - 1556708_1_331.dat - has '.psb' at the beginning, but does not appear to be a valid M2 MArchive/PSB Archive
- - 1556708_2_1976307.dat - One of the ROM packages
- - 1556708_3_150.dat - Small MAME file describing ROM name?
- - 1556708_4_138.dat - Small file of null bytes
- - 1556708_5_667.dat - SCN file - Scene description?
- - 1556708_6_3828.dat - Moderate MMAC file listing ROM file names and possible keys?
- - 1556708_7_26.dat - Very small MMAC file
-
 ### PSB
-In the example above, 1556708_1_331.dat appears to have ".psb" at the beginning; however, this doesn't match what we know about the PSB filetype.
- - If this was an uncompressed PSB file, we would expect the first three bytes to be "PSB"
- - If this were a compressed PSB file, we would expect the first three bytes to be a compression type indicator, like "mdf"
- - We have no current indication M2 (which makes the m2engage emulator and MArchive file format) worked on CAS
- - MArchiveBatchTool can't seem to do anything with the file.
- - The file is far shorter than most PSB files.
-As such, this is likely a coincidence or other dead end.
+See CAS 1 for more info.
     '''
     _out_file_list = [
     ]
 
     _out_file_notes = {
     }
-    _default_input_folder = r"C:\Program Files (x86)\Steam\steamapps\common\Capcom Arcade Stadium"
-    _input_folder_desc = "CAS 1 Folder"
+    _default_input_folder = r"C:\Program Files (x86)\Steam\steamapps\common\Capcom Arcade 2nd Stadium"
+    _input_folder_desc = "CAS 2 Folder"
     _short_description = ""
     _prop_info = {
         "include-partials": {
@@ -76,7 +60,7 @@ As such, this is likely a coincidence or other dead end.
                 if "patch" in filename:
                     logger.info(f"Skipping patch file {filename}")
                 elif filename == "re_chunk_000.pak":
-                    file_id = "1515951"
+                    file_id = "0000000"
                 else:
                     file_id = file[-11:-4]
 
@@ -99,6 +83,6 @@ As such, this is likely a coincidence or other dead end.
                     logger.warning(f'Error while processing {file}!')
                     logger.warning(error)
         else:
-            logger.error("CAS1 is not yet implemented; to help investigate, add --prop 'include-partials=True' to extract what we have so far!")
+            logger.error("cas2 is not yet implemented; to help investigate, add --prop 'include-partials=True' to extract what we have so far!")
 
         logger.info("Processing complete.")
